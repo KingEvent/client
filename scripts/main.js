826d124f33c6d7d25b3fda893a4f345332c806d8
Muhammad Ilham <muhd.iiilham@gmail.com>
2019-11-29 00:21:29 +0700

    Google OAuth added (client side) and Navbar

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,35 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 });
+
+function onSignIn(googleUser) {
+    var id_token = googleUser.getAuthResponse().id_token;
+    $.ajax({
+        method: 'post',
+        url: 'http://localhost:3000/google-signin',
+        data: {
+            id_token: id_token
+        }
+    })
+        .done(userCredentials=> {
+            console.log(userCredentials)
+            $('#sign-in').hide()
+        })
+        .fail(err=> {
+            console.log(err)
+        })
+        .always()
+}
+
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+    $('#sign-in').show()
+}
+
+function loginFunction(e){
+    const data = $('input').serialize()
+}
