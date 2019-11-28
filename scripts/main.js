72164d74b1a000c8f37c85a061a60b62493a49f2
Angela Vanessa <hansavanessa@gmail.com>
2019-11-29 00:25:52 +0700

    add to calendar feature

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,36 @@
+const BASE_URL="http://localhost:3000";
+
 $( document ).ready(function() {
-    console.log( "ready!" );
+    console.log('ready')
+    e = $('#auth-button');
+
+    let options = {
+        'client_id': '9acLp9_J7iFllX9GG3wy9lAgQNAxcYpRUxAikABoAiiFUiHm',
+        'scope': 'google_calendar'
+    };
+
+    Kloudless.auth.authenticator(e, options, function(result) {
+        if (result.error) {
+            console.error('An error occurred:', result.error);
+        } else {
+            sendUserToken(result.access_token);
+        }
+    });
 });
+
+function sendUserToken(token) {
+    $.ajax({
+        type: 'POST',
+        url: `${BASE_URL}/calendar`,
+        data: { token },
+        headers: {
+            authorization: localStorage.getItem('jwt_token')
+        }
+    })   
+    .done(data => {
+        console.log(data)
+    }) 
+    .fail(err => {
+        console.log(err)
+    })
+}
