dd1c3f06b8d1fe6555f5240de5ac25a1a5581395
Muhammad Ilham <muhd.iiilham@gmail.com>
2019-11-29 00:17:39 +0700

    get user ip

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,24 @@
 $( document ).ready(function() {
     console.log( "ready!" );
+    getUserIP()
 });
+
+// get user ip using ip-api.com and saved to localStorage
+function getUserIP() {
+    $.ajax({
+        method: 'get',
+        url: 'http://ip-api.com/json'
+    })
+        .done(ipData=> {
+            let { regionName, city, lat, lon, country } = ipData;
+            localStorage.setItem('city', city)
+            localStorage.setItem('country', country)
+            localStorage.setItem('region', regionName)
+            localStorage.setItem('lat', lat)
+            localStorage.setItem('lon', lon)
+        })
+        .fail(err=> {
+            console.log(err)
+        })
+        .always()
+}
