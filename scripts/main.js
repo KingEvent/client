0e6fd1ae9d20e54449bdd34cea6cd09284d5e1fb
Muhammad Ilham <muhd.iiilham@gmail.com>
2019-11-29 11:45:47 +0700

    navbar

--- scripts/main.js
+++ scripts/main.js
@@ -29,8 +29,9 @@ function onSignIn(googleUser) {
         }
     })
     .done(userCredentials=> {
+        localStorage.setItem('accessToken', userCredentials)
         /* Get user token from server then save to localStorage.accessToken */
-        console.log(userCredentials)
+        $('#logout').show()
         $('#loginModal').modal('hide')
         $('#sign-in').hide()
     })
@@ -40,21 +41,6 @@ function onSignIn(googleUser) {
     .always()
 }
 
-function onSignIn(googleUser) {
-    var id_token = googleUser.getAuthResponse().id_token;
-    $.ajax({
-        method: 'post',
-        url: 'http://localhost:3000/google-signin',
-        data: {
-            id_token: id_token
-        }
-    })
-    .done(userCredentials=> {
-        console.log(userCredentials)
-        $('#loginModal').modal('hide')
-        $('#sign-in').hide()
-    });
-}
 
 // get user ip using ip-api.com and saved to localStorage
 function getUserIP() {
@@ -80,11 +66,14 @@ function getUserIP() {
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
-      console.log('User signed out.');
+        console.log('User signed out.');
+        $('#logout').hide()
+        $('#sign-in').show()
+        localStorage.removeItem('accessToken')
     });
-    $('#sign-in').show()
 }
 
 function loginFunction(e) {
     const data = $('input').serialize()
+    
 }
