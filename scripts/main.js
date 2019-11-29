fe7c1d4b1a70c6eb4f31f19148da00cf8be68b5b
memorvin <alvina.tjia@yahoo.com>
2019-11-29 12:09:37 +0700

    events feature added

--- scripts/main.js
+++ scripts/main.js
@@ -72,8 +72,34 @@ $(document).ready(function () {
         })
     });
 
-
+    $('#search-form').submit(function(e) {
+      e.preventDefault();
+      let value = $('#search-bar').val();
+      $.ajax({
+        url:`http://localhost:5000/events/search/${value}`,
+        type:"get"
+      })
+      .done(data => {
+        console.log(data)
+        viewAll(data)
+      })
+      // .fail((data) => {
+      //   console.log(data);
+      //   alert('gabisa')
+      // }) 
+    })
+    showAll()
 });
+
+function showAll() {
+  $.ajax({
+    url:"http://localhost:5000/events",
+    type:"get"
+  })
+  .done(data => {
+    viewAll(data);
+  })
+}
 
 function askLogin(name) {
     Swal.fire(`Hi ${name},
@@ -169,3 +195,118 @@ function sendUserToken(token) {
         console.log(err)
     })
 }
+
+function showOne(id) {
+  $.ajax({
+    url:`http://localhost:5000/events/${id}`,
+    type:"get"
+  })
+  .done(result => {
+    $('.modal-content').empty()
+    const data = result[0]
+    let venue = (data.entities[0].name) ? data.entities[0].name : "No venue info";
+    let address = (data.entities[0].formatted_address) ? data.entities[0].formatted_address : "No address info";
+    let description = (data.description) ? data.description : "No description";
+    $('.modal-content').append(` 
+    <div class="modal-header">
+      <h1 class="modal-title">${data.title}</h1>
+      <button type="button" class="close" data-dismiss="modal">×</button>
+    </div>
+    <div class="modal-body">
+      <h3>${new Date(data.start).toDateString()}</h3>
+      <h3>${venue}</h3>
+      <h3>${address}</h3>  
+      <p>${description}</p>
+    </div>
+    <div class="modal-footer">
+      <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+    </div>
+    `)
+  })
+}
+
+function viewAll(result) {
+  $('.row').empty()
+  result.forEach(data => {
+    let venue = (data.entities[0].name) ? data.entities[0].name : "No venue info";
+    let address = (data.entities[0].formatted_address) ? data.entities[0].formatted_address : "No address info";
+    $('.row').append(`
+  <div class="col-md-4 col-sm-6 post" id="${data.category}">
+    <div class="card card-block">
+      <h4 class="card-title text-right"><i class="material-icons">${data.category}</i></h4>
+      <h4 class="card-title text-right">
+        <button type="button" onclick="showOne('${data.id}')" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+          Details
+        </button>
+      </h4>
+      <h3 class="card-title mt-3 mb-3"><b>${data.title}</b></h3>
+      <h4 class="card-text">${new Date(data.start).toDateString()}</h4> 
+      <h5 class="card-title mt-3 mb-3">${venue}</h5>
+      <h5 class="card-title mt-3 mb-3">${address}</h5>
+    </div>
+  </div>`)
+  })
+}
+
+$('.post').show();
+  $(".filtering button").on("click", function(){
+    // console.log(this)
+    if (!$(".filtering button").hasClass("active")) {
+      $(".filtering button").addClass("active")
+    }
+    let filtertag = $(this).attr('id');
+    if (filtertag !== 'all') {
+      $('.post').hide().filter('#' + filtertag).show();
+    } else {
+      $('.post').show()
+    }
+  })
+
+  // let rangeSlider = function(){
+  //   let slider = $('.range-slider'),
+  //       range = $('.range-slider__range'),
+  //       value = $('.range-slider__value');
+      
+  //   slider.each(function(){
+  
+  //     value.each(function(){
+  //       let value = $(this).prev().attr('value');
+  //       $(this).html(value);
+  //     });
+  
+  //     range.on('input', function(){
+  //       $(this).next(value).html(this.value);
+  //     });
+  //   });
+  // };
+  // rangeSlider();
+
+  // (function( $ ){
+  //   $( document ).ready( function() {
+  //     $( '.input-range').each(function(){
+  //       var value = $(this).attr('data-slider-value');
+  //       var separator = value.indexOf(',');
+  //       if( separator !== -1 ){
+  //         value = value.split(',');
+  //         value.forEach(function(item, i, arr) {
+  //           arr[ i ] = parseFloat( item );
+  //         });
+  //       } else {
+  //         value = parseFloat( value );
+  //       }
+  //       $( this ).slider({
+  //         formatter: function(value) {
+  //           console.log(value);
+  //           return '$' + value;
+  //         },
+  //         min: parseFloat( $( this ).attr('data-slider-min') ),
+  //         max: parseFloat( $( this ).attr('data-slider-max') ), 
+  //         range: $( this ).attr('data-slider-range'),
+  //         value: value,
+  //         tooltip_split: $( this ).attr('data-slider-tooltip_split'),
+  //         tooltip: $( this ).attr('data-slider-tooltip')
+  //       });
+  //     });
+      
+  //    } );
+  //   } )( jQuery );
